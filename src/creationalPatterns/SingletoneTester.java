0047321d2228b17929788b053a2a99d6527f2789
Ganesh, Om <om.ganesh@emc.com>
2016-04-25 09:50:47 +0530

    Checking in first singletone with out synchronized

---
+++ src/creationalPatterns/SingletoneTester.java
@@ -0,0 +1,35 @@
+package creationalPatterns;
+
+class SingleToneB{
+	static int a = 34;
+			
+}
+
+class SingleTone {
+	static int num = 0;
+	
+		
+	private SingleTone(){
+		System.out.println(" The single tone got intialized");
+	}
+	
+	private static class SingleToneHelper {
+		public static final SingleTone INSTANCE = new SingleTone();
+	}
+	
+	public static SingleTone getInstance(int nuum){
+		num = nuum;
+		return SingleToneHelper.INSTANCE;
+	}
+}
+
+public class SingletoneTester {
+	 
+	static int a =20;
+	
+	public static void main(String args[]){
+		SingleTone.getInstance(5);
+		System.out.println(" value is "+SingleTone.num);
+	}
+
+}
